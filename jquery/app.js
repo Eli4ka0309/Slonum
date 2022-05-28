01e869d7ad03f9771303476d641c27d14aa83874
Eli4ka0309 <100150012+Eli4ka0309@users.noreply.github.com>
2022-05-29 00:03:27 +0300

    add

--- jquery/app.js
+++ jquery/app.js
@@ -1,18 +1,37 @@
-$(function (){
-    let header= $("#header"),
-        introH= $("#intro").innerHeight(),
-        scrollOffset= 0;
+$(function () {
+    let header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
 
+    checkScroll(scrollOffset);
 
-    $(window).on("scroll", function (){
+    $(window).on("scroll", function () {
         scrollOffset = $(this).scrollTop();
 
-        console.log(scrollOffset);
-        if (scrollOffset >= introH){
-            header.addClass("fixed")
-        } else {
-            header.removeClass("fixed")
-        }
+        checkScroll(scrollOffset);
 
     });
+
+    function checkScroll(scrollOffset) {
+        if (scrollOffset >= introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+
+    }
+
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let blockId = $this.data("collapse");
+
+        console.log($(blockId).slideToggle());
+
+        $(blockId).slideUp();
+    });
+
+
+
 });
